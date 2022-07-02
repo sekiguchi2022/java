05f8f90e3a61fb4126782e1456791271f90981fe
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2022-07-02 09:49:45 +0900

    Question02の追加

---
+++ basis/Question02.java
@@ -0,0 +1,28 @@
+package test1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Question2 {
+	public static void main(String[] args) throws IOException {
+
+		//キーボードからの数値入力
+		System.out.println("整数を入力してください");
+		//インスタンス生成
+		BufferedReader a = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = a.readLine();
+		int m = Integer.parseInt(str);
+
+		//コンソールから入力された数値を読み込みint型変数に格納
+		//int m = Integer.parseInt(a.readLine());
+
+		//偶数か奇数判定
+		if (m % 2 == 0) {
+			System.out.println("偶数です");
+		} else {
+			System.out.println("奇数です");
+		}
+	}
+}
