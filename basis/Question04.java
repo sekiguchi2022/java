cd07605129b20d1c37efb820058f5b32a074a226
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2022-07-16 09:48:01 +0900

    旧課題削除

--- basis/Question04.java
+++ basis/Question04.java
@@ -1,43 +1,44 @@
 package test1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Question04 {
-	public static void main(String[] arge) throws IOException {
+	public static void main(String args[]) {
+		//Scanner生成
+		Scanner scanner = new Scanner(System.in);
 
-		try {
-			//インスタンス生成
-			InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-			BufferedReader a = new BufferedReader(inputStreamReader);
+		System.out.println("2つの文字列を入力してください");
+
+		while (true) {
+			//メッセージ出力
+			System.out.println("1個目");
+
+			//一つ目の文字列を入力
+			String input1 = scanner.nextLine();
 
 			//メッセージ出力
-			System.out.println("数字を入力してください");
+			System.out.println("2個目");
 
-			//キーボードから入力された数字を読み込みString型の変数に格納
-			String str = a.readLine();
+			//二つ目の文字列を入力
+			String input2 = scanner.nextLine();
 
-			//読み込んだ文字列をint型の変数に格納
-			int ap1 = Integer.parseInt(str);
-			
-			//入力された数字で100を除算する
-			int ap2 = 100 / ap1;
-			
-			//入力された数字で割り切れた場合
-			System.out.println(ap2);
+			if (input1.equals("") || input2.equals("")) {
+				System.out.println("空白が入力されています");
 
-			//0で除算出来ない場合
-		} catch (ArithmeticException e) {
-			System.out.println("0で除算できません");
-		
-			//数字以外が入力された場合
-		} catch (NumberFormatException e) {
-			System.out.println("数字以外が入力されました");
+				continue;
+			}
+			if (!input1.equals(input2)) {
 
-		} //例外があってもなくても必ず実行する処理
-		finally {
-			System.out.println("終了");
+				System.out.println("違う文字列です");
+
+				continue;
+			}
+			if (input1.equals(input2)) {
+				System.out.println("同じ文字列です");
+
+				break;
+			}
+
 		}
 	}
 }
