1a0edc5353882b8593b86a088f788efbded99813
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2022-10-16 10:52:19 +0900

    Question09

--- basis/Question09.java
+++ basis/Question09.java
@@ -5,65 +5,66 @@ import java.util.Scanner;
 public class Question09 {
 
 	public static void main(String args[]) {
+		try {
+			//Scanner生成
+			Scanner scanner = new Scanner(System.in);
 
-///		int Max = 0;
-//		int Min = 0;
+			//メッセージ出力
+			System.out.println("5つの整数を入力してください");
 
-		//Scanner生成
-		Scanner scanner = new Scanner(System.in);
+			//入力された整数をint型配列arrayに格納
+			int[] array = new int[5];
 
-		//メッセージ出力
-		System.out.println("5つの整数を入力してください");
+			//入力された5つの整数分ループ
+			for (int i = 0; i < 5; i++) {
 
-		//入力された整数をint型配列arrayに格納
-		int[] array = new int[5];
+				//入力された整数に従い個数に1加算
+				System.out.print((i + 1) + "個目:");
 
-		//入力された5つの整数分ループ
-		for (int i = 0; i < 5; i++) {
+				//入力された整数を変換後int型変数に格納
+				int input = Integer.parseInt(scanner.nextLine());
 
-			//入力された整数に従い個数に1加算
-			System.out.print((i + 1) + "個目:");
-
-			//入力された整数を変換後int型変数に格納
-			int input = Integer.parseInt(scanner.nextLine());
-
-			array[i] = input;
-
-		}
-		//配列の中の要素を取り出す
-		//				for (int x : array) {
-		//
-		//					// 配列の中身を表示
-		//					System.out.print(x);
-		//
-		//				}
-
-		//配列の中の要素を昇順にソート
-		for (int i = 0; i < array.length - 1; i++) {
-			for (int j = 0; j < array.length - i - 1; j++) {
-
-				if (array[j] > array[j + 1]) {
-					int tmp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = tmp;
-
-				}
+				array[i] = input;
 
 			}
+			//配列の中の要素を取り出す
+			//				for (int x : array) {
+			//
+			//					// 配列の中身を表示
+			//					System.out.print(x);
+			//
+			//				}
+
+			//配列の中の要素を昇順にソート
+			for (int i = 0; i < array.length - 1; i++) {
+				for (int j = 0; j < array.length - i - 1; j++) {
+
+					if (array[j] > array[j + 1]) {
+						int tmp = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = tmp;
+
+					}
+
+				}
+			}
+			System.out.print("昇順ソート結果は");
+			//debug
+			for (int x : array) {
+				System.out.print(" " + x + " ");
+
+			}
+
+			System.out.println("です");
+
+			int intMax = array[4];
+			int intMin = array[0];
+			System.out.println("最大値は" + " " + intMax + " " + "です");
+			System.out.println("最小値は" + " " + intMin + " " + "です");
+
+		} catch (NumberFormatException e) {
+			//整数以外の値が入力された場合
+			System.out.println("整数以外の値が入力されました");
 		}
-		System.out.print("昇順ソート結果は");
-		//debug
-		for (int x : array) {
-			System.out.print(x);
-
-		}
-
-		System.out.println("です");
-
-		int intMax = array[4];
-		int intMin = array[0];
-		System.out.println("最大値は" + intMax + "です");
-		System.out.println("最小値は" + intMin + "です");
-
 	}
 }
