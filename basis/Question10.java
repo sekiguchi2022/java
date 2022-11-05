e0212ba0796228c8f5c4e408ada34c78b1f05cb6
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2022-11-05 10:46:27 +0900

    Question10.java

---
+++ basis/Question10.java
@@ -0,0 +1,48 @@
+package test1;
+
+import java.util.Scanner;
+
+public class Question10 {
+	public static void main(String args[]) {
+
+		//Scanner生成
+		try (Scanner scanner = new Scanner(System.in);) {
+
+			//メッセージ出力
+			System.out.println("整数を入力してください");
+
+			int input = Integer.parseInt(scanner.nextLine());
+
+			//debug
+			//System.out.println(input);
+
+			//2倍するprintCalculateDoubleメソッドの呼び出し
+			printCalculateDouble(input);
+			//2乗するprintCalculateSquareメソッドの呼び出し
+			printCalculateSquare(input);
+
+			//	System.out.println(ans1);
+			//	System.out.println(ans2);
+
+		}
+
+	}
+
+	//20行目から呼び出されたメソッド(引数なし)
+	public static void printCalculateDouble(int input) {
+
+		int ans1 = input * 2;
+		//引数を2倍した結果を出力する
+		System.out.println("入力された" + " " + input + " " + "を2倍した値は" + " " + ans1 + " " + "です");
+
+	}
+
+	//22行目から呼び出されたメソッド(引数なし)
+	public static void printCalculateSquare(int input) {
+
+		int ans2 = input * input;
+		//引数を2乗した結果を出力する
+		System.out.println("入力された" + " " + input + " " + "を2乗した値は" + " " + ans2 + " " + "です");
+
+	}
+}
