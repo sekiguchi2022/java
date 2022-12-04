ff7b0a5b6f3db64b63f9360d9b7510dee4147d97
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2022-12-04 20:53:04 +0900

    Question12

--- basis/Question12.java
+++ basis/Question12.java
@@ -6,38 +6,51 @@ public class Question12 {
 	public static void main(String args[]) {
 
 		//Scanner生成
-		Scanner scanner = new Scanner(System.in);
+		try (Scanner scanner = new Scanner(System.in);) {
 
-		System.out.println("1：氏名を入力する");
-		System.out.println("2：氏名を入力しない");
-		//メッセージ出力
-		System.out.print("番号を選択してください（1,2）：");
-
-		//メニュー番号の入力
-		int inputNum = Integer.parseInt(scanner.nextLine());
-
-		//氏名番号で分岐
-		if (inputNum == 1) {//氏名が入力された場合
+			System.out.println("1：氏名を入力する");
+			System.out.println("2：氏名を入力しない"
+					+ "");
 			//メッセージ出力
-			System.out.print("氏名を入力してください\n氏名:");
+			System.out.print("番号を選択してください（1,2）：");
 
-			//キーボードから氏名の入力
-			String input = scanner.nextLine();
+			//メニュー番号の入力
+			int inputNum = Integer.parseInt(scanner.nextLine());
 
-			//入力された氏名をPersonインスタンスに設定
-			Person person = new Person(input);
+			//if ((inputNum != 1) || (inputNum != 2)) {
+			if (!(inputNum == 1) && !(inputNum == 2)) {//1,2以外の整数が入力された場合
+				//メッセージ出力
+				System.out.println("1～2の範囲で入力してください");
+			}
 
-			//Personインスタンスの氏名を出力
-			System.out.println("私の名前は" + " " + person.getName()+ " " + "です");
+			//氏名番号で分岐
+			if (inputNum == 1) {//氏名が入力された場合
+				//メッセージ出力
+				System.out.print("氏名を入力してください\n氏名:");
 
-		} else if (inputNum == 2) {//氏名が入力されない場合
-			//"ダミー"をインスタンスに設定
-			Person person = new Person("ダミー");
+				//キーボードから氏名の入力
+				String input = scanner.nextLine();
 
-			//Personインスタンスの氏名を出力
-			System.out.println("私の名前は" + " " + person.getName()+ " " + "です");
+				//入力された氏名をPersonインスタンスに設定
+				Person person = new Person(input);
 
+				//Personインスタンスの氏名を出力
+				System.out.println("私の名前は" + " " + person.getName() + " " + "です");
+
+			} else if (inputNum == 2) {//氏名が入力されない場合
+				//"ダミー"をインスタンスに設定
+				Person person = new Person("ダミー");
+
+				//Personインスタンスの氏名を出力
+				System.out.println("私の名前は" + " " + person.getName() + " " + "です");
+
+			}
+
+		} catch (NumberFormatException e) {
+			//整数以外の値が入力された場合
+			System.out.println("整数以外の値が入力されました");
 		}
 
 	}
+
 }
