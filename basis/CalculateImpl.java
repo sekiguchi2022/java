991532ac4d6acc89077d30736220b3ca4cf4cc71
sekiguchi2022 <sekiguchi_h@hokurin.co.jp>
2023-02-05 18:05:51 +0900

    CalculateImpl

--- basis/CalculateImpl.java
+++ basis/CalculateImpl.java
@@ -4,20 +4,22 @@ package test1;
 public class CalculateImpl implements Calculate {
 
 	public int multiplication(int num1, int num2) {
+
 		//入力された2つ整数を掛け算
 		int ans1 = num1 * num2;
 
 		//引数の掛け算結果をmainクラスに返す
-		return (ans1);
+		return ans1;
 	}
 
 	public int division(int num1, int num2) {
 
-		int ans1 = 0;
+		//検討中
+		//int ans1 = 0;
 		//掛け算結果を2で割る
 		int ans2 = ans1 / 2;
 
 		//引数の掛け算結果を2で割った結果をmainクラスに返す
-		return (ans2);
+		return ans2;
 	}
 }
